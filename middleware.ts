8f5dccb7ca1af22b564602c24948061f6836b0ec
Slimane <benabdallah.slimane@gmail.com>
2023-05-19 11:19:21 +0100

    second commit

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+export {default} from 'next-auth/middleware'
+
+export const config ={
+    matcher:[
+        "/tips",
+        "/reservations",
+        "/properties",
+        "/favorities"
+    ]
+=======
+export {default} from 'next-auth/middleware'
+
+export const config ={
+    matcher:[
+        "/tips",
+        "/reservations",
+        "/properties",
+        "/favorities"
+    ]
+>>>>>>> d7f7b108b4d0c49e16c3376ab52c2ab5c13001d5
+}
