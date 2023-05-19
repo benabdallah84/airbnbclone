2f59b5ab95806f5161f741ee24afe98b82f6f744
Slimane <benabdallah.slimane@gmail.com>
2023-05-19 12:14:42 +0100

    remove ronflict

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import NextAuth, {AuthOptions} from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '@/app/libs/prismadb'
@@ -56,63 +56,4 @@ export const authOptions: AuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET
 }
-=======
-import NextAuth, {AuthOptions} from 'next-auth'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from '@/app/libs/prismadb'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import CredentialsProvider from 'next-auth/providers/credentials'
-//import bcrypt from 'bcryptjs'
-
-
-export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string
-        }), 
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        CredentialsProvider({
-            name:'credentials',
-            credentials: {
-                email: {label: 'email', type: 'text'},
-                password: {label: 'password', type: 'password'},
-            },
-            async authorize(credentials){
-                if(!credentials?.email || !credentials?.password){
-                    throw new Error('Invalid credentials!')
-                }
-                const user = await prisma.user.findUnique({
-                    where:{
-                        email: credentials.email
-                    }
-                })
-                if(!user || !user?.hashPassword){
-                    throw new Error('Invalid credentials!')
-                }
-                 const isCorrectPassword = credentials.password === user.hashPassword //await bcrypt.compare(
-                //     credentials.password, user.hashPassword
-                // )
-                if(!isCorrectPassword){
-                    throw new Error('Invalid credentials!')
-                }
-                return user
-            }
-        })
-    ], 
-    pages:{
-        signIn: '/'
-    },
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: 'jwt'
-    },
-    secret: process.env.NEXTAUTH_SECRET
-}
->>>>>>> d7f7b108b4d0c49e16c3376ab52c2ab5c13001d5
 export default NextAuth(authOptions)
