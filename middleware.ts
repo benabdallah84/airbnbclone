c22323e9c2896c6d35eb66dcc2ad6bc539902790
Slimane <benabdallah.slimane@gmail.com>
2023-05-19 12:26:58 +0100

    remove ronflict in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export {default} from 'next-auth/middleware'
 
 export const config ={
@@ -8,15 +8,5 @@ export const config ={
         "/properties",
         "/favorities"
     ]
-=======
-export {default} from 'next-auth/middleware'
 
-export const config ={
-    matcher:[
-        "/tips",
-        "/reservations",
-        "/properties",
-        "/favorities"
-    ]
->>>>>>> d7f7b108b4d0c49e16c3376ab52c2ab5c13001d5
 }
